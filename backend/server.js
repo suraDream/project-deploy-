819a9ba541f04891b40d0575082171692af6d284
suraDreame <surachai.up@rmuti.ac.th>
2025-06-27 17:08:07 +0700

    ทดจะทดdeployและใข้cloud storage แก้ env

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
 
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin:'*',
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
